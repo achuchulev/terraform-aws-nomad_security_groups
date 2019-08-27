bfbd612e7bb0329432d343da70547d2618723cd4
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-08-27 17:16:35 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "security_group_nomad_traffic" {
 }
 
 output "security_group_ssh_traffic" {
-  value = aws_security_group.allow_nomad_ssh_traffic_sg.id
+  value = aws_security_group.allow_nomad_ssh_traffic.id
 }
 
 output "security_group_icmp_traffic" {
-  value = aws_security_group.allow_nomad_icmp_traffic_sg.id
+  value = aws_security_group.allow_nomad_icmp_traffic.id
 }
